fe3bead044b0e2909b144a96091b792da9ea4310
Tien-code <80895788+Tien-code-pro@users.noreply.github.com>
2021-11-26 15:36:23 +0700

    WAD

---
+++ T2010A_WAD/T2010A_WAD/Controllers/BrandController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace T2010A_WAD.Controllers
+{
+    public class BrandController : Controller
+    {
+        // GET: Brand
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateCategory()
+        {
+            return View();
+        }
+        public ActionResult DeleteCategory()
+        {
+            return View();
+        }
+    }
+}
---
+++ T2010A_WAD/T2010A_WAD/Controllers/CategoriesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using T2010A_WAD.Models;
+
+namespace T2010A_WAD.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private DataContext context = new DataContext();
+        // GET: Categories
+        public ActionResult Index()
+        {
+            ViewData["Page-Title"] = "Categories page";
+            ViewBag.PageTitle = "Demo Title for categories Page";
+            // var category = new Category() { Id = 1, CategoriesName = "Fashion" };
+            // ViewBag.Category = category;
+            var list = context.Categories.ToList();
+            return View(list); //passing data by model
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateCategory()
+        {
+            return View();
+        }
+        public ActionResult DeleteCategory()
+        {
+            return View();
+        }
+    }
+}
